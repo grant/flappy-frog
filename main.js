f3bd3641cf117536ea5e297b71758e4eddf165bb
Grant Timmerman <granttimmerman@gmail.com>
2014-02-03 23:53:55 -0800

    Added basic menu interaction

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 $(function () {
+	var FADE_DURATION = 100;
 	var $menu = $('.menu');
 	var $getReady = $('.getReady');
 	var $gameover = $('.gameover');
 
+	var inGame = false;
+	var isReady = false;
+	var score = 0;
+	var best = 0;
 	$menu.show();
+
+	// Button clicks
+	$menu.find('button').click(function () {
+		$menu.fadeOut(FADE_DURATION, function () {
+			$getReady.find('.instructions').show();
+			$getReady.fadeIn(FADE_DURATION, function () {
+				isReady = true;
+			});
+		});
+	});
+
+	// Tap click
+	$('.container').click(function () {
+		if (!inGame && isReady) {
+			// Start game
+			inGame = true;
+			isReady = false;
+			$getReady.find('.instructions').fadeOut(FADE_DURATION);
+		}
+	});
 });
