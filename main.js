863df734fcbf516e6c3d714b849761538a41710f
Grant Timmerman <granttimmerman@gmail.com>
2014-02-04 01:10:05 -0800

    Added basic gravity for frog

--- main.js
+++ main.js
@@ -28,5 +28,52 @@ $(function () {
 			isReady = false;
 			$getReady.find('.instructions').fadeOut(FADE_DURATION);
 		}
+
+		if (inGame) {
+			vy -= CLICK_POWER;
+			console.log(vy);
+		}
 	});
+
+	// Game
+	var CLICK_POWER = 2;
+	var vy = 0;
+	var y = 0;
+	var g = 0.05;
+	$frog = $('.frog');
+	function render() {
+		if (inGame) {
+			calculateWorld();
+			renderWorld();
+		}
+	}
+
+	// Calculates stuff for the world
+	function calculateWorld () {
+		vy += g;
+		if (vy > 1) {
+			vy = 1;
+		}
+		y += vy;
+	}
+
+	// Renders the current world
+	function renderWorld () {
+		$frog.css('top', y + '%');
+	}
+
+	// shim layer with setTimeout fallback
+	window.requestAnimFrame = (function(){
+		return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		function(callback){
+			window.setTimeout(callback, 1000 / 60);
+		};
+	})();
+
+	(function animloop(){
+		requestAnimFrame(animloop);
+		render();
+	})();
 });
